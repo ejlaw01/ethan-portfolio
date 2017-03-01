a7f79540f4402ee559176fd0953ace53b8719a27
Ethan Law <ejlaw01@gmail.com>
2017-03-01 00:20:43 -0800

    add fade in and out to contact button content and background image to porject sections

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
-  $("#contact").click(function(){
-    $(".title-detail").fadeOut('slow', function(){
-      $(".contact").fadeIn('slow');
-    });
+  var counter = 1;
+  $('#contact').click(function(){
+    var self = this;
+    if (counter%2 === 0) {
+      $(".contact").fadeToggle('slow', function(){
+        $(".title-detail").fadeToggle('slow');
+        counter += 1;
+      });
+    } else {
+      $(".title-detail").fadeToggle('slow', function(){
+        $(".contact").fadeToggle('slow');
+        counter += 1;
+      });
+    }
+    console.log(counter);
   });
-  // , function(){
-  //   $(".contact").fadeOut('fast', function(){
-  //     $(".title-detail").fadeIn();
-  //   });
-  // });
+
+  $(document).on('click', 'a.scroll', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 300);
+  });
 });
