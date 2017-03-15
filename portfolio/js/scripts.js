3401ada26568ff014bb346644ff659719e34e3bc
Ethan Law <ejlaw01@gmail.com>
2017-03-14 23:46:21 -0600

    reformat project sections

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -1,3 +1,19 @@
+
+// var developer = function() {
+//   for (var day = 0; day < 7; day++) {
+//     if (day <= 5) {
+//       alarm(hitSnooze());
+//       drinkMate(yerba.bitter, water.hot);
+//       code(Javascript(JQuery || Angular2), HTML5, CSS3);
+//     } else {
+//       dAndD(roll(20));
+//       goHiking(nature);
+//       dance(beats);
+//     }
+//   }
+// }
+
+
 $(document).ready(function(){
   var counter = 1;
   $('#contact').click(function(){
@@ -22,4 +38,12 @@ $(document).ready(function(){
       scrollTop: $( $.attr(this, 'href') ).offset().top+.5
     }, 500);
   });
+  
+  // $('#hey-im-ethan').mouseover(function(){
+  //   $('#ethan-detail').css('display', 'block');
+  // });
+  // $('#hey-im-ethan').mouseout(function(){
+  //   $('#ethan-detail').css('display', 'none');
+  // });
+
 });
