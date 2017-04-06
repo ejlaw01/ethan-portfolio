f51b8061695e7b98453742ada8d3c75bcb9c8ef6
Ethan Law <ejlaw01@gmail.com>
2017-04-06 16:29:11 -0700

    restyle contact section

--- portfolio/js/scripts.js
+++ portfolio/js/scripts.js
@@ -17,24 +17,6 @@
 
 $(document).ready(function(){
 
-  var counter = 1;
-  $('#contact').click(function(){
-    var self = this;
-    if (counter%2 === 0) {
-      $(".contact").fadeToggle('slow', function(){
-        $(".title-detail").fadeToggle('slow');
-        counter += 1;
-      });
-    } else {
-      $(".title-detail").fadeToggle('slow', function(){
-        $(".contact").fadeToggle('slow');
-        counter += 1;
-      });
-    }
-    console.log(counter);
-  });
-
-
   $(document).on('click', 'a.scroll', function(event) {
     event.preventDefault();
     $('html, body').animate({
